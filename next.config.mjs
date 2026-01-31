80919ef4cb10dcc9fd8bd5c050b58b6c35729516
Igor Unguryanov <igrom4ikus@gmail.com>
2026-02-01 00:37:14 +0200

    fix: restore output export for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
     '127.0.0.1',
     '*.localhost',
   ],
-  output: undefined,
+  output: isGitHubPages ? 'export' : undefined,
   basePath: isGitHubPages ? '/igrom-3d-environment.ru' : '',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
