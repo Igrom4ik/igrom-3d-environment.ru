92829809160ccde4dd24181ae62f69e10547b415
Igor Unguryanov <igrom4ikus@gmail.com>
2026-01-31 04:48:09 +0200

    Update admin header design to match site liquid glass style and use JetBrains Mono

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,10 +5,18 @@ import "@/resources/custom.css";
 import { Providers } from "@/components";
 import AdminNav from "@/components/admin/AdminNav";
 import { Column, Flex } from "@once-ui-system/core";
+import { fonts } from "@/resources";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Flex as="html" lang="en" fillWidth direction="column" suppressHydrationWarning>
+    <Flex 
+      as="html" 
+      lang="en" 
+      fillWidth 
+      direction="column" 
+      suppressHydrationWarning
+      className={`${fonts.heading.variable} ${fonts.body.variable} ${fonts.label.variable} ${fonts.code.variable}`}
+    >
       <Providers>
         <Column
           as="body"
--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -39,7 +39,7 @@ const protectedRoutes: ProtectedRoutesConfig = {
 
 // Import and set font for each variant
 import { Geist } from "next/font/google";
-import { Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 
 const heading = Geist({
   variable: "--font-heading",
@@ -59,7 +59,7 @@ const label = Geist({
   display: "swap",
 });
 
-const code = Geist_Mono({
+const code = JetBrains_Mono({
   variable: "--font-code",
   subsets: ["latin"],
   display: "swap",
